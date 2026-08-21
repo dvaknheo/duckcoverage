539981bc404443f1f778bbf5683fab0b3130d074
dvaknhao <dvakneho@qq.com>
2026-08-21 15:58:08 +0800

    兼容性测试由LibCoverage 保证

--- docker/test-php84/stop-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-if docker ps --format '{{.Names}}' | grep -q '^duckcoverage-test84$'; then
-    docker stop duckcoverage-test84
-    echo "duckcoverage-test84 stopped"
-else
-    echo "duckcoverage-test84 is not running"
-fi
